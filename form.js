7332e2b8a80ea7a959c4cd0dfe7c5e2eb45d0acd
Shehan De Zoysa <84965737+shehandezoysa@users.noreply.github.com>
2024-01-30 00:32:56 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,23 @@
+
+var email = document.forms['vform']['email'];
+email.addEventListener('blur', emailVerify, true);
+
+
+  // validate email
+  if (email.value == "") {
+    email.style.border = "1px solid red";
+    document.getElementById('email_div').style.color = "red";
+    email_error.textContent = "Email is required";
+    email.focus();
+    return false;
+  }
+  
+
+function emailVerify() {
+  if (email.value != "") {
+  	email.style.border = "1px solid #5e6e66";
+  	document.getElementById('email_div').style.color = "#5e6e66";
+  	email_error.innerHTML = "";
+  	return true;
+  }
+}
